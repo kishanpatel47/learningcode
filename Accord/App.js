f225cd52163e7ee3a9c8b40f5cc90c590d02144e
kishanpatel47 <kishandudhatra804@gmail.com>
2023-08-03 10:58:10 +0530

    imagepicker

--- Accord/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import MyNavigator from './Route';
-const App = () => {
-  return <MyNavigator />;
-};
-
-export default App;
--- Accord/Component/Untils/String.js
+++
@@ -1,6 +0,0 @@
-export const String = {
-  search: 'Search  a Doctor or Health issuse',
-  searchCity: 'Search City or Locality',
-  specialisedservice: 'Specialised Service',
-  advancedcare: '24/7 Advanced care',
-};
